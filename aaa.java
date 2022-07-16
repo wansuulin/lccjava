93cc03ee080af5955a9306444e9628f6ac0d2f02
wansuulin <109008282+wansuulin@users.noreply.github.com>
2022-07-16 22:48:05 +0800

    作業1

---
+++ aaa.java
@@ -0,0 +1,17 @@
+package student;
+
+public class aaa {
+	
+	public static void main(String[] args) {
+	java.util.Scanner scan = new java.util.Scanner(System.in);
+		
+	System.out.println("歡迎來到快樂餐廳，請點餐(A、B、C):");
+	
+	int price = 0;
+	
+	String num = scan.next();
+	
+	
+	}
+
+}
