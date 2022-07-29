380de9afd6364f2a56a66d1ea8511c8f1427f9d5
wansuulin <109008282+wansuulin@users.noreply.github.com>
2022-07-29 20:57:11 +0800

    作業3

---
+++ cc_2.java
@@ -0,0 +1,21 @@
+package sfsgvs;
+
+public class cc_2 {
+	public static void main(String[] args) {
+		for(int i= 1 ; i <= 5 ; i++) {
+			for(int x=1 ; x <= i ; x++) {
+				System.out.print(i);
+			}
+			System.out.println();
+			
+		}
+		for(int i= 4 ; i >= 1 ; i--) {
+			for(int x=1 ; x <= i ; x++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		
+	}
+	
+}
+}
