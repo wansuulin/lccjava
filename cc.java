bd1a6cd77ef8bf5a7b325c87d3269abfaf22f874
wansuulin <109008282+wansuulin@users.noreply.github.com>
2022-07-29 20:45:14 +0800

    作業2

---
+++ cc.java
@@ -0,0 +1,21 @@
+package student;
+
+public class cc {
+	public static void main(String[] args) {
+		
+		for(int i = 1 ; i <= 9 ; i++) {
+			
+			for(int x = 2 ; x <= 9 ; x++) {
+				
+				
+				System.out.print(x + "*" + i + "=" + i*x + "\t");
+				
+			}
+			System.out.println();
+			
+			
+		}
+		
+	}
+
+}
